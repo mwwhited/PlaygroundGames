fecae7b467b4eef802b327ebe6222df7b7082453
Matthew Whited <matt.whited@lightwellinc.com>
2023-04-22 09:06:34 -0400

    pull in pge example

--- src/cpp/olc/ExampleGames/OlcExample1/OlcExample1.cpp
+++ src/cpp/olc/ExampleGames/OlcExample1/OlcExample1.cpp
@@ -2,8 +2,40 @@
 // https://medium.com/@viveik.chauhan/how-i-bypass-crowdstrike-restriction-1bc558abd464
 
 #include <iostream>
+#include <olcPixelGameEngine/olcPixelGameEngine.h>
+
+
+// Override base class with your custom functionality
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		// Name your application
+		sAppName = "Example";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Called once per frame, draws random coloured pixels
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		return true;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello Worlkd!\n";
+	Example demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+	return 0;
 }
